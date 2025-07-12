e3f271952cbfebbfe7f59aa1294d6adb865df6ca
Titouan-Esc <t.escorneboueu@gmail.com>
2025-07-12 21:41:12 +0200

    Implement http part for routing API REST

--- internal/delivery/http.go
+++ internal/delivery/http.go
@@ -1,1 +1,17 @@
 package delivery
+
+import (
+	"github.com/Gym-Partner/api_common/database"
+	"github.com/Gym-Partner/user-service/internal/controller"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
+func RegisterRoutes(router *gin.Engine, db *database.Database) {
+	newController := controller.New(db)
+
+	v1 := router.Group(viper.GetString("API_PREFIX"))
+	{
+		v1.POST("/user/create", newController.Create)
+	}
+}
