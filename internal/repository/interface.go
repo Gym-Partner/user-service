ac0601c06e63a00121090a07893c77a297355bd2
Titouan-Esc <t.escorneboueu@gmail.com>
2025-07-12 21:29:03 +0200

    Implement repository part without content

--- internal/repository/interface.go
+++ internal/repository/interface.go
@@ -1,1 +1,11 @@
 package repository
+
+import (
+	"github.com/Gym-Partner/api_common/serviceError"
+	"github.com/Gym-Partner/user-service/internal/domain"
+)
+
+type IRepository interface {
+	IsExist(data, OPT string) bool
+	Create(data domain.User) (domain.User, *serviceError.Error)
+}
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,23 @@
 package repository
+
+import (
+	"github.com/Gym-Partner/api_common/logger"
+	"github.com/Gym-Partner/api_common/serviceError"
+	"github.com/Gym-Partner/user-service/internal/domain"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	DB  *gorm.DB
+	Log *logger.Logger
+}
+
+func (r Repository) IsExist(data, OPT string) bool {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r Repository) Create(data domain.User) (domain.User, *serviceError.Error) {
+	//TODO implement me
+	panic("implement me")
+}
