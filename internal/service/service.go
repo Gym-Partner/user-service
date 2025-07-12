a247f2c836a1e78e034fb7d1f8e3b2ad6977d01b
Titouan-Esc <t.escorneboueu@gmail.com>
2025-07-13 00:20:37 +0200

    User create -> work

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,8 +1,10 @@
 package service
 
 import (
+	"fmt"
 	"github.com/Gym-Partner/api_common/serviceError"
 	"github.com/Gym-Partner/api_common/utils"
+	"github.com/Gym-Partner/user-service/internal/constants"
 	"github.com/Gym-Partner/user-service/internal/domain"
 	"github.com/Gym-Partner/user-service/internal/repository"
 	"github.com/gin-gonic/gin"
@@ -14,6 +16,22 @@ type Service struct {
 }
 
 func (s Service) Create(ctx *gin.Context) (domain.User, *serviceError.Error) {
-	//TODO implement me
-	panic("implement me")
+	userPtr, err := s.Utils.InjectBodyInModel(ctx)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	exist := s.IRepository.IsExist(userPtr.Email, "email")
+	if exist {
+		return domain.User{}, serviceError.New(
+			serviceError.HttpCode400,
+			fmt.Sprintf(constants.ServiceErrAppINTUserAlreadyExist, userPtr.Email))
+	}
+
+	user, err := s.IRepository.Create(*userPtr)
+	if err != nil {
+		return domain.User{}, err
+	}
+
+	return user, nil
 }
