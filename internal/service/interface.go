a66a94a151a8f7ab7fb48919ee0bfe12801ac8e8
Titouan-Esc <t.escorneboueu@gmail.com>
2025-07-12 21:25:54 +0200

    Implement service part without content

--- internal/service/interface.go
+++ internal/service/interface.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"github.com/Gym-Partner/api_common/serviceError"
+	"github.com/Gym-Partner/user-service/internal/domain"
+	"github.com/gin-gonic/gin"
+)
+
+type IService interface {
+	Create(ctx *gin.Context) (domain.User, *serviceError.Error)
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,17 @@
 package service
+
+import (
+	"github.com/Gym-Partner/api_common/serviceError"
+	"github.com/Gym-Partner/api_common/utils"
+	"github.com/Gym-Partner/user-service/internal/domain"
+	"github.com/gin-gonic/gin"
+)
+
+type Service struct {
+	Utils utils.Utils[domain.User]
+}
+
+func (s Service) Create(ctx *gin.Context) (domain.User, *serviceError.Error) {
+	//TODO implement me
+	panic("implement me")
+}
