5e088f77d914736c1cc0dba94c71b60a236527e0
Titouan-Esc <t.escorneboueu@gmail.com>
2025-07-13 09:18:08 +0200

    Upgrade user domain part

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -4,22 +4,49 @@ import (
 	"github.com/Gym-Partner/api_common/serviceError"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"time"
 )
 
 type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	ID        string     `json:"id" swaggerignore:"true"`
+	FirstName string     `json:"first_name" example:"Oscar"`
+	LastName  string     `json:"last_name" example:"Escorneboueu"`
+	Username  string     `json:"username" example:"don_oscar_anton"`
+	Email     string     `json:"email" example:"titouan.esc@icloud.com"`
+	Phone     string     `json:"phone" example:"+33672135172"`
+	Password  string     `json:"password" example:"aaaAAA111"`
+	Followers []string   `json:"followers,omitempty" gorm:"-"`
+	Following []string   `json:"following,omitempty" gorm:"-"`
+	Image     string     `json:"image,omitempty" gorm:"-"`
+	CreatedAt *time.Time `json:"created_at"`
 }
 type Users []User
 
 func (u *User) Response() gin.H {
 	return gin.H{
 		"data": gin.H{
-			"username": u.Username,
-			"email":    u.Email,
+			"id":         u.ID,
+			"first_name": u.FirstName,
+			"last_name":  u.LastName,
+			"username":   u.Username,
+			"email":      u.Email,
+			"phone":      u.Phone,
+			"followers":  u.Followers,
+			"following":  u.Following,
+			"image":      u.Image,
 		},
+	}
+}
+
+func (u *Users) Response() gin.H {
+	var result []gin.H
+
+	for _, user := range *u {
+		result = append(result, user.Response())
+	}
+
+	return gin.H{
+		"data": result,
 	}
 }
 
@@ -36,11 +63,17 @@ func (u *User) HashPassword(hashFunc func(string) (string, *serviceError.Error))
 	return nil
 }
 
+// ----------------------------- SQL -----------------------------
+
 type MigrateUser struct {
-	ID       string `gorm:"primaryKey; not null"`
-	Username string `gorm:"column:username; not null"`
-	Email    string `gorm:"not null"`
-	Password string `gorm:"not null"`
+	ID        string `gorm:"primaryKey; not null"`
+	FirstName string
+	LastName  string
+	Username  string `gorm:"column:username; not null"`
+	Email     string `gorm:"not null"`
+	Phone     string
+	Password  string    `gorm:"not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
 
 func (MigrateUser) TableName() string { return "users" }
